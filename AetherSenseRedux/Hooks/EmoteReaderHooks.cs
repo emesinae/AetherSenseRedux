325d9bcfdc503ce8d67b883a509c8cb12303a9e3
Cool Bug <psilidae@outlook.com>
2025-04-19 02:26:17 -0400

    wip: emit events for any emotes involving local pc

--- AetherSenseRedux/Hooks/EmoteReaderHooks.cs
+++ AetherSenseRedux/Hooks/EmoteReaderHooks.cs
@@ -1,5 +1,3 @@
-namespace AetherSenseRedux.Hooks;
-
 // The original implementation of this class was originally
 // from the _Pat Me_ and _Emote Log_ plugins.
 //
@@ -16,52 +14,92 @@ using Dalamud.Hooking;
 using System;
 using System.Linq;
 
-public class EmoteReaderHooks : IDisposable
+namespace AetherSenseRedux.Hooks
 {
-    public delegate void EmoteDelegate(IPlayerCharacter playerCharacter, ushort emoteId);
-
-    public event EmoteDelegate? OnEmote;
-
-    private delegate void OnEmoteFuncDelegate(ulong unk, ulong instigatorAddr, ushort emoteId, ulong targetId, ulong unk2);
-    private readonly Hook<OnEmoteFuncDelegate> hookEmote;
-
-    public EmoteReaderHooks()
+    public class EmoteReaderHooks : IDisposable
     {
-        hookEmote = Service.GameInteropProvider.HookFromSignature<OnEmoteFuncDelegate>("E8 ?? ?? ?? ?? 48 8D 8B ?? ?? ?? ?? 4C 89 74 24", OnEmoteDetour);
-        hookEmote.Enable();
-    }
+        public delegate void EmoteDelegate(EmoteEvent e);
 
-    public void Dispose()
-    {
-        hookEmote?.Dispose();
-    }
+        public event EmoteDelegate? OnEmote;
 
-    private void OnEmoteDetour(ulong unk, ulong instigatorAddr, ushort emoteId, ulong targetId, ulong unk2)
-    {
-        // unk - some field of event framework singleton? doesn't matter here anyway
-        Service.PluginLog.Info($"Emote >> unk:{unk:X}, instigatorAddr:{instigatorAddr:X}, emoteId:{emoteId}, targetId:{targetId:X}, unk2:{unk2:X}, player:{Service.ClientState.LocalPlayer?.GameObjectId:X}");
+        private delegate void OnEmoteFuncDelegate(ulong unk, ulong instigatorAddr, ushort emoteId, ulong targetId,
+            ulong unk2);
 
-        if (Service.ClientState.LocalPlayer != null)
+        private readonly Hook<OnEmoteFuncDelegate> hookEmote;
+
+        public EmoteReaderHooks()
         {
-            var instigatorOb = Service.ObjectTable.FirstOrDefault(x => (ulong)x.Address == instigatorAddr);
-            if (instigatorOb is IPlayerCharacter playerCharacter)
+            hookEmote = Service.GameInteropProvider.HookFromSignature<OnEmoteFuncDelegate>(
+                "E8 ?? ?? ?? ?? 48 8D 8B ?? ?? ?? ?? 4C 89 74 24", OnEmoteDetour);
+            hookEmote.Enable();
+        }
+
+        public void Dispose()
+        {
+            hookEmote?.Dispose();
+        }
+
+        private void OnEmoteDetour(ulong unk, ulong instigatorAddr, ushort emoteId, ulong targetId, ulong unk2)
+        {
+            // unk - some field of event framework singleton? doesn't matter here anyway
+            Service.PluginLog.Info(
+                $"Emote >> unk:{unk:X}, instigatorAddr:{instigatorAddr:X}, emoteId:{emoteId}, targetId:{targetId:X}, unk2:{unk2:X}, player:{Service.ClientState.LocalPlayer?.GameObjectId:X}");
+
+            if (Service.ClientState.LocalPlayer != null)
             {
-                if (targetId == Service.ClientState.LocalPlayer.GameObjectId)
+                var instigatorOb = Service.ObjectTable.FirstOrDefault(x => (ulong)x.Address == instigatorAddr);
+                if (instigatorOb is IPlayerCharacter playerCharacter)
                 {
-                    OnEmote?.Invoke(playerCharacter, emoteId);
-                }
-                else if (instigatorOb.GameObjectId == Service.ClientState.LocalPlayer.GameObjectId)
-                {
-                    Service.PluginLog.Info($"Local player {instigatorOb.Name} used emote {emoteId} on target {targetId:X}");
-                }
-                else
-                {
-                    Service.PluginLog.Info($"I DONT KNOW {instigatorOb.GameObjectId:X} {instigatorOb.Name}");
+                    // If a remote player performed the emote while targeting the local player
+                    if (targetId == Service.ClientState.LocalPlayer.GameObjectId)
+                    {
+                        Service.PluginLog.Debug($"Player {instigatorOb.Name} used emote {emoteId} on target {Service.ClientState.LocalPlayer.Name} ({targetId:X})");
+                        OnEmote?.Invoke(new EmoteEvent
+                        {
+                            EmoteId = emoteId,
+                            Instigator = playerCharacter,
+                            Target = Service.ClientState.LocalPlayer
+                        });
+                    }
+                    // If the local player performed the emote
+                    else if (instigatorOb.GameObjectId == Service.ClientState.LocalPlayer.GameObjectId)
+                    {
+                        var targetOb = targetId != 0xE0000000
+                            ? Service.ObjectTable.FirstOrDefault(x => (ulong)x.GameObjectId == targetId)
+                            : null;
+
+                        Service.PluginLog.Debug(
+                            $"Local player {instigatorOb.Name} used emote {emoteId} on target {targetOb?.Name?.ToString() ?? "NONE"} ({targetId:X})");
+
+                        OnEmote?.Invoke(new EmoteEvent
+                        {
+                            EmoteId = emoteId,
+                            Instigator = Service.ClientState.LocalPlayer,
+                            Target = (targetOb is IPlayerCharacter targetPc) ? targetPc : null
+                        });
+                    }
                 }
             }
 
+            hookEmote.Original(unk, instigatorAddr, emoteId, targetId, unk2);
         }
+    }
 
-        hookEmote.Original(unk, instigatorAddr, emoteId, targetId, unk2);
+    public class EmoteEvent
+    {
+        /// <summary>
+        /// The character which performed the emote.
+        /// </summary>
+        public IPlayerCharacter Instigator { get; set; }
+
+        /// <summary>
+        /// The character who was the target of the emote.
+        /// </summary>
+        public IPlayerCharacter? Target { get; set; } = null;
+
+        /// <summary>
+        /// The emote ID.
+        /// </summary>
+        public ushort EmoteId { get; set; }
     }
 }
